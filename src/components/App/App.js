65d4406969a38d50f440536ec5d53d5216f2a694
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-25 15:35:48 -0700

    Change class names to camelcase

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -58,14 +58,6 @@ class App extends Component {
     this.setState({ allergenFriendly: allergenFriendly });
   }
   
-
-  // what's data type are we getting from the api call? -> array of objects with property tags (array of strings) which we want to filter through to create category arrays.
-
-  // in the API call, is that where we want the logic of separating categories to occur?
-  // componentDidMount, API calls and category array assignments
-  // does it need to be in the api call?
-  // this.state.makeup is then split
-
   render() {
     return (
       <main>
@@ -73,30 +65,41 @@ class App extends Component {
       <Route exact path='/' render={() => {
         return (
           <div className="App">
-            <div className="title-container">
+            <div className="titleContainer">
               <h1>FaceIt</h1>
               <Logo />
-              <h2 class="mission-statement">Discover beauty products that compliment our lifestyles.</h2>
+              <h2 className="missionStatement">
+                Discover beauty products that compliment our lifestyles.
+              </h2>
             </div>
-            <section className="category-container">
+            <section className="categoryContainer">
               <Link to="eco">
-                <article 
-                  className="mainCategory" 
-                  style={{backgroundImage: `url(${"https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1748&q=80"})`}}>
+                <article
+                  className="mainCategory"
+                  style={{
+                    backgroundImage: `url(${"https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1748&q=80"})`,
+                  }}
+                >
                   <h3 className="mainCategoryText">Eco</h3>
                 </article>
               </Link>
               <Link to="allergenFriendly">
-                <article 
-                  className="mainCategory" 
-                  style={{backgroundImage: `url(${"https://images.unsplash.com/photo-1591130219388-ae3d1c17431b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=633&q=80"})`}}>
+                <article
+                  className="mainCategory"
+                  style={{
+                    backgroundImage: `url(${"https://images.unsplash.com/photo-1591130219388-ae3d1c17431b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=633&q=80"})`,
+                  }}
+                >
                   <h3 className="mainCategoryText">Fair Trade</h3>
                 </article>
               </Link>
               <Link to="vegan">
-                <article 
-                  className="mainCategory" 
-                  style={{backgroundImage: `url(${"https://images.unsplash.com/photo-1509298271096-c979b9203fd7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1212&q=80"})`}}>
+                <article
+                  className="mainCategory"
+                  style={{
+                    backgroundImage: `url(${"https://images.unsplash.com/photo-1509298271096-c979b9203fd7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1212&q=80"})`,
+                  }}
+                >
                   <h3 className="mainCategoryText">Vegan</h3>
                 </article>
               </Link>
