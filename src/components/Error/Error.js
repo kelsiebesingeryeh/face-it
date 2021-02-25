f1546c5a31dafa7d926bc41d064b70bd314e24dc
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-25 11:51:47 -0500

    Include container around text for styling purposes

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -5,9 +5,11 @@ import './Error.css';
 const Error = () => {
   return (
     <section className='error-message'>
-      <h2>We're having technical difficulties.</h2>
-      <p><em>Please refresh or revisit at a later time.</em></p>
-      <h3>Thank you.</h3>
+      <article className='message'>
+        <h2>We're having technical difficulties.</h2>
+        <p><em>Please refresh or revisit at a later time.</em></p>
+        <h3>Thank you.</h3>
+      </article>
     </section>
   )
 }
