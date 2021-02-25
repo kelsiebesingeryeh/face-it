81fee865b175b74bff2a61592316f3ec44cdc2e6
conconartist <conconart.info@gmail.com>
2021-02-24 19:34:39 -0700

    Update layout in Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,35 @@ import React from 'react';
 import './Footer.css';
 import { Link } from 'react-router-dom';
 
-function Footer() {
+const Footer = () => {
     return (
        <footer>
-
+           <h4>FaceIt</h4>
+             <h5>is brought to you by</h5>
+              <div className="dev-container">
+                <div className="dev">
+                  <p>Kelsie Besinger Yeh</p>
+                  <div className="links">
+                    <a href="https://github.com/kelsiebesingeryeh/">Github</a>
+                    <a href="https://www.linkedin.com/in/kelsie-besinger-yeh-80947132/">LinkedIn</a>
+                  </div>
+                </div>
+                <div className="dev">
+                  <p>Connie Hong </p>
+                  <div className="links">
+                    <a href="https://github.com/conconartist">Github</a>
+                    <a href="https://www.linkedin.com/in/connie-h-hong/">LinkedIn</a>
+                    <a href="https://twitter.com/conconartist?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Twitter</a>
+                  </div>
+                </div>
+                <div className="dev">
+                  <p>Gabrielle Joyce</p>
+                  <div className="links">
+                    <a href="https://github.com/gaj23">Github</a>
+                    <a href="https://www.linkedin.com/in/gaj23/">LinkedIn</a>
+                  </div>
+                </div>
+              </div>
        </footer> 
     )
 }
