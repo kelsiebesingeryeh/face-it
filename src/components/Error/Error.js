2d250bbbf896df48e7e01a6bd7b8cd88fff06520
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-25 08:34:11 -0500

    Apply background image to match comp styling

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import error-img from './error-img.jpg';
+import error from './error-img.jpg';
 import './Error.css';
 
 const Error = () => {
   return (
     <section className='error-message'>
-      <img src={error-img} alt='broken blush dics' />
       <h2>We're having technical difficulties.</h2>
-      <h3>Please refresh or revisit at a later time. Thank you.</h3>
+      <p><em>Please refresh or revisit at a later time.</em></p>
+      <h3>Thank you.</h3>
     </section>
   )
 }
