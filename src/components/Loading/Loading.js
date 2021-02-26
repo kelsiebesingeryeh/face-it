b4c14a3978e8b6050309fd525192e00479a4ac7c
conconartist <conconart.info@gmail.com>
2021-02-26 14:41:56 -0700

    Refactor LoadingMessage component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './Loading.css';
-import LoadingImg from './LoadingImg.svg';
-
-//render when fetching components
-//add/ hide component in componentDidMount?
-//is LoadingImg best practice or should I build a component for the svg like i did for the logo?
+import LoadingImg from '../LoadingImg/LoadingImg';
 
 const LoadingMessage = () => {
     return (
         <section className='loading-message'>
             <h2 className='loading-text'>Gathering...</h2>
-            <img src={LoadingImg}></img>
+            <LoadingImg />
         </section>
     )
 }
