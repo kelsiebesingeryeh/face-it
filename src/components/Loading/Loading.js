2797436e40a56f09df30cdb1543cef0f8e66ee54
conconartist <conconart.info@gmail.com>
2021-02-25 20:38:37 -0700

    Fixed line change

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Loading.css';
 import LoadingImg from './LoadingImg.svg';
 
+//render when fetching components
+//add/ hide component in componentDidMount?
+//is LoadingImg best practice or should I build a component for the svg like i did for the logo?
+
 const LoadingMessage = () => {
     return (
         <section className='loading-message'>
