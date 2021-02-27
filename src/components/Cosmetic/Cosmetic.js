ca556bb73a2e5160ca4a594d411e86840a42c727
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-27 09:31:14 -0700

    Add card details for product type

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Cosmetic.css';
+import { Link } from "react-router-dom";
 
-const Cosmetic = () => {
+const Cosmetic = ({name, brand, img, tags, productType, id}) => {
   return (
-    <h3>A cosmetic</h3>
+    <article className="productType"
+    style={{ backgroundImage: `url(${img})` }}
+    >
+      
+    </article>
   )
 }
 
 export default Cosmetic;
+
+
+// const Type = ({ img, title, category }) => {
+//   return (
+//     <Link to={`/${category}/${title}`}>
+//       <article
+//         className="productType"
+//         style={{ backgroundImage: `url(${img})` }}
+//       >
+//         <h3>{title}</h3>
+//       </article>
+//     </Link>
+//   );
+// };
+
+// export default Type;
--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
+import './Items.css';
 
 const Items = ({ data, type }) => {
     const cosmeticTypes = data.filter(item => item['product_type'] === type)
+    console.log('data', data)
+    console.log('type', type)
     const cosmeticToDisplay = cosmeticTypes.map(item => {
-      return <Cosmetic />
+      return <Cosmetic
+      id={item.id}
+      key={item.id}
+      name={item.name}
+      brand={item.brand}
+      img={item["image_link"]}
+      tags={item["tag_list"]}
+      productType={item["product_type"]} 
+      />
     })
     //we have filtered arrays; next question ...
     //map over items (array of objects)
     //and return an array of details
     //cosemetic = card
     return (
-        <div>{cosmeticToDisplay}</div>
+        <div className="productContainer">
+          {cosmeticToDisplay}
+        </div>
     )
 }
 
