d80e5f172fcb9fdb162fef908dcd7b00e853ca34
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-27 12:18:50 -0700

    Add cosmetic styling for individual cards?

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -3,36 +3,18 @@ import './Cosmetic.css';
 import { Link } from "react-router-dom";
 
 const Cosmetic = ({name, brand, img, tags, productType, id}) => {
-  const styledTags = tags.join(', ')
+  const styledTags = tags.join(', ');
 
   return (
-    <>
       <article className="cosmeticCard">
         <img src={img} alt={name} className="cosmeticCardImage"></img>
-        <p>{brand}</p>
-        <p>{name}</p>
-        <span>
-          <p>{styledTags}</p>
-        </span>
+        <p className="cosmeticDetail">
+          <strong>{brand}</strong>
+        </p>
+        <p className="cosmeticDetail">{name}</p>
+        <p className="tags">{styledTags}</p>
       </article>
-    </>
   );
 }
 
 export default Cosmetic;
-
-
-// const Type = ({ img, title, category }) => {
-//   return (
-//     <Link to={`/${category}/${title}`}>
-//       <article
-//         className="productType"
-//         style={{ backgroundImage: `url(${img})` }}
-//       >
-//         <h3>{title}</h3>
-//       </article>
-//     </Link>
-//   );
-// };
-
-// export default Type;
