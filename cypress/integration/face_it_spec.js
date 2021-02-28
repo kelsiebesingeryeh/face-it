418f8ce55250e145b34bdf5ea4bef1ab03d59332
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-27 21:15:39 -0500

    Add testing for category and footer

--- cypress/integration/face_it_spec.js
+++ cypress/integration/face_it_spec.js
@@ -18,16 +18,25 @@ describe('FaceIt', () => {
       cy.get('nav').should('be.visible')
     })
 
-    it('Should have a title, logo, and mission statement', () => {
+    it('Should have a title, and mission statement', () => {
       cy.get('nav').contains('Home').click()
       cy.get('.App > .titleContainer').should('contain', 'FaceIt').and('contain', 'Discover')
-
     })
 
+    it('Should have a section with three categories displayed', () => {
+      cy.get('.App section a article h3').should('contain', 'Eco').and('contain', 'Allergen').and('contain', 'Vegan')
+    })
 
+    it('Should have a footer with links', () => {
+      cy.get('footer').should('be.visible');
+      cy.get('footer div div p').contains('Kelsie')
+      //edit for images in links?
+      cy.get('footer div div div a')
+      cy.get('footer div div div a').should('have.attr', 'href').should('be.equal', 'https://github.com/kelsiebesingeryeh/')
+    })
   })
 
-  describe.only('Nav Bar', () => {
+  describe('Nav Bar', () => {
     beforeEach(() => {
       cy.fixture('mockData.json')
         .then(makeup => {
@@ -51,8 +60,8 @@ describe('FaceIt', () => {
         .get('.App').should('be.visible')
     })
 
-    //Add checks for contents 
-    it.only('Should navigate to the eco page', () => {
+    //Add checks for contents
+    it('Should navigate to the eco page', () => {
       cy.get('nav').contains('Eco').click()
     })
 
@@ -62,6 +71,23 @@ describe('FaceIt', () => {
 
     it('Should navigate to the vegan page', () => {
       cy.get('nav').contains('Vegan').click()
+    })
+  })
+
+  describe('Nav Bar', () => {
+    beforeEach(() => {
+      cy.fixture('mockData.json')
+        .then(makeup => {
+          console.log(makeup);
+          cy.intercept('http://localhost:3001/api/v1/makeup/', {
+            body: makeup
+          })
+        });
+      cy.visit(baseURL)
+    })
+
+    it('Should be able to click into a category', () => {
+      
     })
   })
 })
