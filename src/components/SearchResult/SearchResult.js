ccba0301926ef15264dd53b0e2799976ef2860ac
conconartist <conconart.info@gmail.com>
2021-02-28 19:37:45 -0700

    Add SearchResult component

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SearchResult.css';
+import { Link } from "react-router-dom";
+
+const SearchResult = ({img, name, id, brand, tags, productType}) => {
+    const styledTags = tags.join(', ');
+    console.log(styledTags)
+    return (
+        <Link to={`/:id`}>
+            <article className="cosmeticCard">
+              <img src={img} alt={name} className="searchImage" id={id}></img>
+              <p className="searchResultDetail">
+              <strong>{brand}</strong>
+              </p>
+              <p className="searchResultDetail">{name}</p>
+              <p className="tags">{styledTags}</p>
+              <p className="products">{productType}</p>
+            </article>
+        </Link>
+    );
+}
+export default SearchResult;
