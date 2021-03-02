0763fe45b9fe04e1438e5cdd58ab84151deb38f7
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-02 14:56:10 -0700

    Add error handling to details page

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SingleProduct from '../SingleProduct/SingleProduct';
 import './Details.css';
+import { Redirect } from "react-router-dom";
 
 const Details = ({ makeup, id }) => {
+  console.log('makeup', makeup)
+
     const findProduct = () => {
         return makeup.find((item) => item.id === parseInt(id));
     } 
 
     const detailsOnDisplay = () => {
         const ourProduct = findProduct()
+        console.log('ourProduct', ourProduct)
+        if (makeup.length === 0) {
+          return <Redirect to="/error" />;
+        }
         return (
           <SingleProduct
             id={ourProduct.id}
@@ -21,9 +28,11 @@ const Details = ({ makeup, id }) => {
             tags={ourProduct["tag_list"]}
             website={ourProduct["website_link"]}
           />
-        );}
-
-  return <div className="productDetailsContainer">{detailsOnDisplay()}</div>;
+        )}
+    
+    return <div className="productDetailsContainer">{detailsOnDisplay()}</div>;
+      
+  
 };
 
 export default Details;
